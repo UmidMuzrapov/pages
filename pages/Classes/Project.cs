b7dd0f2ecb507f757820761815105627dd4cac7c
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-12-16 15:22:29 -0700

    Initial commit

---
+++ pages/Classes/Project.cs
@@ -0,0 +1,11 @@
+﻿
+public class Project
+{
+    public int? Id { get; set; }
+    public string? Image { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool? LiveTest { get; set; }
+    public string? Tools { get; set; }
+
+}
