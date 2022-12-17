40116c56f0f8350731f6d667eb176c0c70ff5776
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-12-17 11:59:58 -0700

    Add grid for courses

---
+++ pages/Classes/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace pages.Classes
+{
+    public class Course
+    {
+        public string Name { get; set; } = "";
+        public string Grade { get; set; } = "";
+        public string Description { get; set; } = "";
+
+    }
+}
+
