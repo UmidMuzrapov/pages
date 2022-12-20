3a871faec041d7b5927d918a123204f83c37a6ec
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2022-12-19 18:06:40 -0700

    Add skill class

---
+++ pages/Classes/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace pages.Classes
+{
+    public class Skill
+    {
+        public string Name { get; set; } = "";
+        public int Level { get; set; } = 100;
+        public string Image { get; set; } = "";
+    }
+}
