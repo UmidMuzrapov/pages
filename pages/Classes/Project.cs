b354f5d967422f7349fb42f9fd23f7eb9edc7ce1
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2023-08-20 17:43:06 -0700

    Add option for a live demo

--- pages/Classes/Project.cs
+++ pages/Classes/Project.cs
@@ -5,7 +5,8 @@ public class Project
     public string? Image { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public bool? LiveTest { get; set; }
+    public bool LiveTest { get; set; }
+    public string? LiveTestLink { get; set; }
     public string? Tools { get; set; }
     public string? GitLink { get; set; }
     public string? Category { get; set; }
